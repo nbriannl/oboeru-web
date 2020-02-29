0056204b4402a3bdf8708a07f5a2f43d3ddc5bb7
Labayna Neil Brian Narido <neilbrian.nl@gmail.com>
2020-02-29 23:17:35 +0800

    Updates

---
+++ precache-manifest.22408d67c15e09b5f64cbbb35d4ed487.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14f0234b104316c330d04dbbfe460a56",
+    "url": "/oboeru-web/index.html"
+  },
+  {
+    "revision": "17ac75444228f4f0e21a",
+    "url": "/oboeru-web/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "123c3b42ebf41eea6118",
+    "url": "/oboeru-web/static/js/2.1b8bcbfe.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/oboeru-web/static/js/2.1b8bcbfe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "17ac75444228f4f0e21a",
+    "url": "/oboeru-web/static/js/main.4dfe76db.chunk.js"
+  },
+  {
+    "revision": "847ebd3bf8fdd8f02a12",
+    "url": "/oboeru-web/static/js/runtime-main.42dea728.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/oboeru-web/static/media/logo.5d5d9eef.svg"
+  }
+]);
