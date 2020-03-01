2e979fc5a965689ddae9b6247ffeaf199a0ea89b
Labayna Neil Brian Narido <neilbrian.nl@gmail.com>
2020-03-01 14:30:45 +0800

    Make Static mock

--- src/App.js
+++ src/App.js
@@ -1,8 +1,64 @@
 import React from "react";
+import {
+  Row,
+  Col,
+  Container,
+  Alert,
+  Badge,
+  Navbar,
+  Card,
+  Button,
+  ButtonGroup
+} from "react-bootstrap";
 
 import "./App.css";
 
-const App = () => <b></b>;
+const App = () => (
+  <div>
+    <Navbar bg="danger" variant="dark" expand="lg">
+      <Navbar.Brand href="#home">Oboeru</Navbar.Brand>
+    </Navbar>
+    <Container>
+      <Card className="text-center">
+        <Card.Header>
+          Question <Badge variant="secondary">2</Badge> out of{" "}
+          <Badge variant="secondary">50</Badge>
+        </Card.Header>
+        <Card.Body>
+          <Card.Title>See you later</Card.Title>
+          <Card.Text>( ? )[。]</Card.Text>
+          <ButtonGroup vertical>
+            <Button variant="outline-danger">おさがしですか</Button>
+            <Button variant="outline-danger">しつれいします</Button>
+            <Button variant="outline-danger">行ってきます</Button>
+            <Button variant="outline-danger">つかれました</Button>
+          </ButtonGroup>
+        </Card.Body>
+        <Card.Footer>
+          <Container>
+            <Row>
+              <Col>
+                <Alert variant="success">
+                  <i className="fa fa-check"></i>{" "}
+                  <Badge variant="success">2</Badge>
+                </Alert>
+              </Col>
+              <Col>
+                <Alert variant="danger">
+                  <i className="fa fa-times"></i>{" "}
+                  <Badge variant="danger">0</Badge>
+                </Alert>
+              </Col>
+              <Col>
+                <Button variant="outline-danger">Quit</Button>
+              </Col>
+            </Row>
+          </Container>
+        </Card.Footer>
+      </Card>
+    </Container>
+  </div>
+);
 
 export default App;
 
